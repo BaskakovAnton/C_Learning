6457cfa4c9162fa7332f3d64ea0733d501904c5e
BaskakovAnton <8Bazz8@mail.ru>
2023-09-21 09:33:44 +0300

    edit Leson008_ArrayLibrary/Program.cs

--- Leson008_ArrayLibrary/Program.cs
+++ Leson008_ArrayLibrary/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections;
+using System.Collections.ObjectModel;
+using System;
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index<length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        System.Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int indexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while(index <count)
+    {
+        if(collection[index]==find)
+        {
+            position = index;
+            break;
+        }    
+        index++; 
+    }
+    return position;
+}
+
+
+int [] Array = new int[10];
+
+FillArray(Array);
+PrintArray(Array);
+System.Console.WriteLine();
+
+int pos = indexOf(Array, 4);
+System.Console.WriteLine(pos);
