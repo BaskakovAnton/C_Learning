003e632413ad3f98bb5377d02b1b014a90af5ff8
BaskakovAnton <8Bazz8@mail.ru>
2023-09-21 09:10:58 +0300

    add Leson008_ArrayLibrary/Lesson007_MethodArray/

---
+++ Lesson007_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+int[] Array = { 1, 12, 31, 4, 15, 16, 4, 18 };
+
+int n = Array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if (Array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        System.Console.WriteLine(Array[index]);
+        break;
+    }
+    index++;
+}
