405cded60098d7acb146324228379f49ed969677
BaskakovAnton <8Bazz8@mail.ru>
2023-09-29 11:42:33 +0300

    add Lesson009

--- Lesson009_functions/Program.cs
+++ Lesson009_functions/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Threading;
+// // вид 1
+
+// void Method1()
+// {
+//     System.Console.WriteLine("Автор ...");
+// }
+
+// Method1(); //вызов метода
+
+// //вид 2
+// void Method2(string msg)
+// {
+//     System.Console.WriteLine(msg);
+// }
+
+// Method2(msg: "Текст сообщения")
+
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while(i < count)
+//     {
+//         System.Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+
+// Method21(msg: "Текст", count: 4);
+
+
+//вид3
+
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+
+// int year = Method3();
+// System.Console.WriteLine(year);
+
+
+//вид4
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(count: 10, text: "a");
+Console.WriteLine(res);
---
+++ Lesson009_functions2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 0; j < 10; j++)
+//     {
+//         System.Console.WriteLine($"{i} * {j} = {i * j}");
+//     }
+//     System.Console.WriteLine();
+// }
+
+//============ Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить черточками, 
+// маленькие буквы заменить большими "К",
+// а большие "С" заменить маленькими "с".
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             012345
+//             s[3] = r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+
+System.Console.WriteLine(newText);
+System.Console.WriteLine();
+
+newText = Replace(newText, 'к', 'К');
+
+System.Console.WriteLine(newText);
+System.Console.WriteLine();
+
+newText = Replace(newText, 'С', 'с');
+
+System.Console.WriteLine(newText);
+System.Console.WriteLine();
